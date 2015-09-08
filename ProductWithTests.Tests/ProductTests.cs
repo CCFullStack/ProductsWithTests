f0b4f8b19077b4dab496d8ec78946d6b1cf88de9
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-09-08 17:40:22 -0500

    Product with Tests:

---
+++ ProductWithTests.Tests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductWithTests.Models;
+using System.Linq;
+
+namespace ProductWithTests.Tests {
+
+    [TestClass]
+    public class ProductTests {
+
+        [TestMethod]
+        public void TestProductNameRequired() {
+
+            // Arrange
+            var product = new Product {
+                Name = "",
+                Price = 3.33m
+            };
+
+            // Act
+            var results = TestHelper.Validate(product);
+
+            // Assert
+            Assert.IsTrue(results.Any(p => p.ErrorMessage == "Name is required."));
+        }
+
+        [TestMethod]
+        public void TestPriceNot17() {
+
+        }
+    }
+}
